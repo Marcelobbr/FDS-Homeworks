202b14bf1923c1f7152c8b1b549ed70d3faa81dd
Marcelo Barata Ribeiro <marcelobbribeiro@gmail.com>
2019-07-27 18:42:20 -0300

    old version of sql funcs

--- hw1/update_papers.py
+++ hw1/update_papers.py
@@ -4,6 +4,7 @@
 import sqlite3
 from bs4 import BeautifulSoup
 import requests
+import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -15,6 +16,9 @@ class UpdateTools:
     def update(self, papers_raw, initialize=True):
         if papers_raw is None: return
         
+        ###################################################
+        #                   PART 1
+        ###################################################
         soup = BeautifulSoup(papers_raw, 'html.parser')
         soup.findAll("td", {"class": "gsc_a_t"})
     
@@ -23,19 +27,19 @@ class UpdateTools:
         for tr in table.find_all('tr')[2:]:
             for td in tr.find_all("td", {"class": "gsc_a_t"}):
                 paper = {}
-                text = re.sub("'", "", tr.find("a", {"class": "gsc_a_at"}).get_text())
+                text = re.sub("'", "", tr.find("a", {"class": "gsc_a_at"}).get_text()).strip()
                 paper['title'] = text
                 authors = tr.find("div", {"class": "gs_gray"}).get_text().split(',')[:4]
                 authors = [a.strip().upper() for a in authors] #remove espaçamento antes de alguns nomes e resolve case sensitiveness
                 paper['authors'] = authors
                 papers.append(paper)
-        papers = papers[:10]
+        papers = papers[:10] #CHANGE BEFORE SUBMISSION
         print(papers)
     
         ###################################################
         #                   PART 2
         ###################################################
-        # ### authors
+        # ### AUTHORS # ###
         author_paper = pd.DataFrame(papers, columns=['title', 'authors'])
     
         authors_table = author_paper.authors.apply(pd.Series).stack(dropna=True).reset_index(drop=True)
@@ -44,26 +48,29 @@ class UpdateTools:
         authors_table = authors_table.sort_values('author')
         authors_table = authors_table.reset_index().drop('index', axis=1)
     
-        # ### papers
-        papers_table = author_paper['title']
+        # ### PAPERS # ###
+        #papers_table = author_paper['title'] # old style
+        papers_table = author_paper['title'].apply(pd.Series).reset_index(drop=True)
+        papers_table = papers_table.rename(columns={0: 'title'})
+        #papers_table.head()
     
-        # ### SQL
+        # ### SQL # ###
     
         sql_db = 'hw1.sqlite'
-        conn = sqlite3.connect(sql_db)
+        conn = sqlite3.connect(sql_db, isolation_level=None)
         cur = conn.cursor()
     
-        if initialize = True:
-            def build_sql_table(dataframe, table_name):  
-                dataframe.to_sql(table_name, conn, index_label = "id", if_exists="replace")
-                print('Table',table_name, 'was created on database', sql_db)
+        #if initialize == True:
+        def build_sql_table(dataframe, table_name):  
+            dataframe.to_sql(table_name, conn, index_label = "id", if_exists="replace")
+            print('Table',table_name, 'was created on database', sql_db)
     
         build_sql_table(authors_table, 'authors')
         build_sql_table(papers_table, 'papers')
         cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
         cur.fetchall()  
     
-        # ### author_paper
+        # ### AUTHOR PAPER # ###
         author_paper_table = author_paper.authors.apply(pd.Series) \
             .merge(author_paper, left_index = True, right_index = True) \
             .drop(["authors"], axis = 1) \
@@ -72,77 +79,55 @@ class UpdateTools:
             .dropna()
     
         #remove espaçamento nos nomes, se houver
-        author_paper_table = pd.DataFrame(author_paper_table.author.apply(lambda x: x.strip())) \
-            .merge(author_paper_table[['title']], left_index = True, right_index = True)
+        #author_paper_table = pd.DataFrame(author_paper_table.author.apply(lambda x: x.strip())) \
+        #    .merge(author_paper_table[['title']], left_index = True, right_index = True)
     
         cur.execute('''CREATE TABLE IF NOT EXISTS author_paper 
                        (title_id INT, author_id INT)
                        ;''')
     
         for index, row in author_paper_table.iterrows():
-            title = row[1] #TEM QUE TROCAR CASO NAO REMOVA ESPAÇAMENTO
-            author = row[0] #TEM QUE TROCAR CASO NAO REMOVA ESPAÇAMENTO
+            title = row[0] #TEM QUE TROCAR CASO REMOVA ESPAÇAMENTO
+            author = row[1] #TEM QUE TROCAR CASO REMOVA ESPAÇAMENTO
     
             query = 'SELECT id FROM papers WHERE title = "{}"'.format(title)
             cur.execute(query)
             title_id = cur.fetchone()[0]
             query = 'SELECT id FROM authors WHERE author = "{}"'.format(author)
-            #print(query)
             cur.execute(query)
             author_id = cur.fetchone()[0]
     
-            #print(title_id, author_id)
             query = "INSERT INTO author_paper VALUES ({}, {})".format(title_id,author_id)
             cur.execute(query)
             conn.commit()
-        if initialize = False:
+        #if initialize = False:
             
         # # build networks
         # ### authors
     
-        G=nx.Graph()
-        authors = pd.read_sql_query('SELECT * FROM authors',conn)
-        labels={}
-        for index,row in authors.iterrows():
-            G.add_node(row[0])
-            labels[row[0]]=row[1]
-    
-        author_paper = pd.read_sql_query('SELECT * FROM author_paper',conn)
-        author_paper = author_paper.sort_values(by='title_id')
-    
-        graph = list(author_paper.groupby(['title_id'])['author_id'].apply(list))
-        for group in graph:
-            if len(group)<2: continue
-            for i in range(0,len(group)-1):
-                for j in range(i+1,len(group)):
-                    G.add_edge(group[i],group[j])
-    
-        pos=nx.spring_layout(G, k=0.2, weight=1, iterations=50)
-        plt.figure(3,figsize=(12,12)) 
-        nx.draw_networkx(G, pos, labels=labels, alpha=0.7)
-        plt.axis('off')
-        plt.savefig("static/authors_graph.png")
-    
-        # ### papers
-        G_papers=nx.Graph()
-        papers = pd.read_sql_query('SELECT * FROM papers',conn)
-        labels={}
-        for index,row in papers.iterrows():
-            G_papers.add_node(row[0])
-            labels[row[0]]=row[1]
-    
-        author_paper = pd.read_sql_query('SELECT * FROM author_paper',conn)
-        author_paper = author_paper.sort_values(by='author_id')
-    
-        graph = list(author_paper.groupby(['author_id'])['title_id'].apply(list))
-        for group in graph:
-            if len(group)<2: continue
-            for i in range(0,len(group)-1):
-                for j in range(i+1,len(group)):
-                    G_papers.add_edge(group[i],group[j])
-    
-        pos=nx.spring_layout(G_papers, k=0.2, weight=1, iterations=50)
-        plt.figure(3,figsize=(12,12)) 
-        nx.draw_networkx(G_papers, pos, labels=labels, alpha=0.7)
-        plt.axis('off')
-        plt.savefig("static/papers_graph.png")
+        def plot_networks(table, aggregator, grouped_id):
+            G=nx.Graph()
+            df = pd.read_sql_query('SELECT * FROM {}'.format(table),conn)
+            labels={}
+            for index,row in df.iterrows():
+                G.add_node(row[0])
+                labels[row[0]]=row[1]
+        
+            author_paper = pd.read_sql_query('SELECT * FROM author_paper',conn)
+            author_paper = author_paper.sort_values(by='title_id')
+        
+            graph = list(author_paper.groupby([aggregator])[grouped_id].apply(list))
+            for group in graph:
+                if len(group)<2: continue
+                for i in range(0,len(group)-1):
+                    for j in range(i+1,len(group)):
+                        G.add_edge(group[i],group[j])
+        
+            pos=nx.spring_layout(G, k=0.2, weight=1, iterations=50)
+            plt.figure(3,figsize=(12,12)) 
+            nx.draw_networkx(G, pos, labels=labels, alpha=0.7)
+            plt.axis('off')
+            file = "static/{}_graph.png".format(table)
+            plt.savefig(file)
+        plot_networks('authors', 'title_id', 'author_id')
+        plot_networks('papers', 'author_id', 'title_id')
