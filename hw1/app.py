68fc14aebe24c116a2e87d7c9ba1388e7336d061
Marcelo Barata Ribeiro <marcelobbribeiro@gmail.com>
2019-07-28 10:14:27 -0300

    starting to add cache control

--- hw1/app.py
+++ hw1/app.py
@@ -1,34 +1,41 @@
 """
 TO RUN THIS CODE on terminal:
-export FLASK_APP=app.py
-flask run
-OR
-python app.py
+    export FLASK_APP=app.py
+    flask run
+    OR
+    python app.py
 """
-#import os
-import sqlite3
+#import sqlite3
 from bs4 import BeautifulSoup
 import requests
-from flask import Flask, render_template,request,g, redirect
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
+from flask import Flask, render_template,request,g, redirect
+from flask_cache  import Cache
 
 #self-made tools
 import scrape_scholar
 from scrape_scholar import scrape
 import update_papers
 from update_papers import UpdateTools
-#import csv
 
 app = Flask(__name__)
-#DATABASE = 'hw1.sqlite'
+
+cache = Cache(app,config={'CACHE_TYPE': 'null'})
+#app.config["CACHE_TYPE"] = "null"
+
+# change to "redis" and restart to cache again
+
+# some time later
+cache.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #author = request.args.get('Jeffrey Heer')    
-    #result = scrape('Jeffrey Heer')
-    #UpdateTools().update(result) #will initialize database and plot graphs on browser
+    cache.clear()
+    author = request.args.get('Jeffrey Heer')    
+    result = scrape('Jeffrey Heer')
+    UpdateTools().update(result) #will initialize database and plot graphs on browser
     return render_template("index.html")
     
 @app.route('/action_page.php')
@@ -43,42 +50,18 @@ def scrape2():
     return "Hello World"
     
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)#, host='0.0.0.0')
+    app.run(debug=True, port=5000)
     
-    
-#DRAFTS
 """
-#using sql: https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
+References:
+# https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
 # https://www.tutorialspoint.com/flask/flask_sqlite
+# html button: https://www.w3schools.com/bootstrap/bootstrap_navbar.asp
+    #check for navbar forms
+
+#DRAFTS
 # /action_page.php?author=Jeffrey+Heer
-    
-    #file = "test.csv"
-    #writer = csv.writer(open(file, 'w'))
-    #writer.writerow(author)
-    
-    #if db is None:
-    #    db = g._database = sqlite3.connect(DATABASE)
-    #db = g._database = sqlite3.connect(DATABASE)
-    #print (type(db))
-
-@app.route("/")
-def index():
-    return render_template("index.html");
-
-@app.route('/scrape',methods = ['POST'])
-def scraper():
-    x = request.form['author'];
-    sc = scrape(x);
-    return render_template("index.html", message=sc);
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True);
     
-    
-@app.route("/scrape")
-def scrape():
-    mars = mongo.db.mars 
-    mars_data = scrape_mars.scrape()
-    mars.update({}, mars_data, upsert=True)
-    return redirect("http://localhost:5000/", code=302)
 """
